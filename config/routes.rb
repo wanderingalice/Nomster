17327b0fd8bde96ef781d2a965d618a1cd4bb581
Alice Duff <duff.alice@gmail.com>
2019-07-09 00:00:54 +0000

    updated gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'places#index'
+  resources :places
 
 end
