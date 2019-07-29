25c03adfa529643640c8438218172c4363cff263
Alice Duff <duff.alice@gmail.com>
2019-07-29 06:22:59 +0000

    added carousel

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,7 @@ class NotificationMailer < ApplicationMailer
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
+    @comment_owner = comment.user
 
     mail(to: @place_owner.email,
           subject: "A comment has been added to #{@place.name}!")
