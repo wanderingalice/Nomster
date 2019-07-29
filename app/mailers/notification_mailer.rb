fd3804fbba8b336be93a9d9f78fe2fe189e4ca64
Alice Duff <duff.alice@gmail.com>
2019-07-29 01:59:01 +0000

    can now send emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
+  default from: "no.reply.wanderingaliceapps@gmail.com"
 
   def comment_added
     mail(to: "duff.alice@gmail.com",
